0c9cf57bc544cd170f0e83df2dd6ed2e5dc0a98e
yar_shto <jarik13012000@gmail.com>
2025-01-25 12:50:12 +0000

    Lesson ~18 or something

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=15)
+    email = models.EmailField()
+    photo = models.ImageField(upload_to='contacts_photo/', blank=True, null=True)
+
 # Create your models here.
