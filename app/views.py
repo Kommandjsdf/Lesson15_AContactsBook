dbf1f21b3fb24d173b66a4f2be8ee0e2d5666d14
yar_shto <jarik13012000@gmail.com>
2025-02-08 12:48:35 +0000

    wrote what I didnt do + lesson

--- app/views.py
+++ app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Contact
+
+def contact_list(request):
+    contacts = Contact.object.all()
+    return render(request, 'contact_list.html', {'contacts': contacts})
+
 
 # Create your views here.
