5fc79ed7e6f72eb7a2550ba59f79deb00218ba67
yar_shto <jarik13012000@gmail.com>
2025-02-15 12:53:04 +0000

    Lesson, design

--- app/views.py
+++ app/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Contact
+from .forms import ContactsForm
 
 def contact_list(request):
     contacts = Contact.objects.all()
     return render(request, 'contact_list.html', {'contacts': contacts})
 
+def add_contact(request):
+    if request.method == 'POST':
+        form = ContactsForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('contact_list')
+    else:
+        form = ContactsForm()
+        return render(request, 'contact_form.html')
 
 # Create your views here.
--- project/settings.py
+++ project/settings.py
@@ -117,9 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'app/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+MEDIA_URL = 'app/media/'
+
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
