002c73606c9e592590dd1c64bd19d448ad28a4e2
yar_shto <jarik13012000@gmail.com>
2025-02-15 11:03:02 +0000

    *objects

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Contact
 
 def contact_list(request):
-    contacts = Contact.object.all()
+    contacts = Contact.objects.all()
     return render(request, 'contact_list.html', {'contacts': contacts})
 
 
