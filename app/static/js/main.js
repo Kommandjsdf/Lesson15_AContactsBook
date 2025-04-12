6f9e95470d43dfedc7328d61b0e631a5511e0979
yar_shto <jarik13012000@gmail.com>
2025-04-12 11:28:39 +0100

    a lot

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -2,8 +2,8 @@ document.addEvenListener("DOMContentLoaded", function(){
   const editButton = document.getElementById("editButton");
   let selectedRow = null;
   let selectedContactId = null;
-  const row = document.querySelectorAll(".table-element");
-  rows forEach(row => {
+  const row = document.querySelectorAll(".contact-row");
+  rows.forEach(row => {
     row.addEventListener("click", function{
       if (selectedRow){
         selectedRow.classList.remove("table-active");
@@ -13,7 +13,7 @@ document.addEvenListener("DOMContentLoaded", function(){
 
       selectedContactId = this.dataset.contactId;
 
-      editButton.href = `/edit/${selectedContactId}`
+      editButton.href = `edit/${selectedContactId}/`
 
     });
   });
